e11716ee532e0945e5af3f2b8164b966dc7eabcb
MarsuGrimpant <marquet.quentin@live.fr>
2016-03-28 15:03:26 +0900

    Real 2_1 file

--- lab2_1.cpp
+++ lab2_1.cpp
@@ -1,15 +1,29 @@
 // Do not remove the include below
+#define DUTY_CYCLE 20
+#define	FRONT_LED_PIN	10
+#define	REAR_LED_PIN	9
 #include "lab2_1.h"
 
 
-//The setup function is called once at startup of the sketch
-void setup()
-{
-// Add your initialization code here
+void myAnalogWrite(int pin,	int percent,	int time)	{
+	//int pin:	pin	number
+	//int percent:	brightness	 0%	~	100%
+	//int time:	brightness	 remains	the	same	during	 this	time	in	ms
+	for (int ncycle = 0 ; ncycle < time/DUTY_CYCLE ; ++ncycle){
+		digitalWrite(REAR_LED_PIN,HIGH);
+		delay(percent * DUTY_CYCLE / 100);
+		digitalWrite(REAR_LED_PIN,LOW);
+		delay((100-percent) * DUTY_CYCLE / 100);
+	}
 }
+void setup(){
+	pinMode(REAR_LED_PIN,	OUTPUT);
 
-// The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+}
+void loop(){
+	myAnalogWrite(REAR_LED_PIN,100,2000);
+	myAnalogWrite(REAR_LED_PIN,75,2000);
+	myAnalogWrite(REAR_LED_PIN,50,2000);
+	myAnalogWrite(REAR_LED_PIN,25,2000);
+	myAnalogWrite(REAR_LED_PIN,0,2000);
 }
